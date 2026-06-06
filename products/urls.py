0214b1c017648197dc6143c3bb429bf82f9d7af7
Artem Maltsev <artemmaltsevwork@gmail.com>
2026-06-06 18:18:59 +0300

    Initial commit

--- products/urls.py
+++ products/urls.py
@@ -6,3 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.products, name='products'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
